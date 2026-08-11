5f8d0393eaead0036352e94eb9b10301f160121b
shale <slucas001@citymail.cuny.edu>
2026-08-11 01:56:21 -0400

    building out utility functions for ports, devices, etc

--- utils.c
+++ utils.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 
 #include <linux/if.h>
+#include <linux/if_tun.h>
 
 #include "utils.h"
 #include "brx.h"
 
-// Create a tap
+// Create a tap device
 int tap_alloc (char *name) {
 
 	struct ifreq ifr;
@@ -26,6 +31,11 @@ int tap_alloc (char *name) {
 	if (*name) strncpy(ifr.ifr_name, name, IFNAMSIZ);
 	if ((ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ) { goto error; }
 
+	// XXX: For a persistent tap interface, note that if brx ends, the virtual bridge 
+	// disappears so the tap interfaces are left in kernel state, so they must be 
+	// cleaned up !!!
+	// if (ioctl(fd, TUNSETPERSIST, 1) < 0) goto error;
+
 	strncpy(name, ifr.ifr_name, IFNAMSIZ); 
 	return fd;
 
@@ -42,14 +52,16 @@ int handle_bridge (char * arguments[], int length) {
 	const char* object = arguments[0];
 
 	if (strncmp(object, "create", MAX_BUFFER) == 0) {
-		return create_bridge (); 
+		// return create_bridge (); 
 	} else if (strncmp (object, "show", MAX_BUFFER) == 0) {
-		return show_bridge (); 
+		// return show_bridge (); 
 	} else if (strncmp (object, "delete", MAX_BUFFER) == 0) {
-		return remove_bridge ();
-	} 
+		// return remove_bridge ();
+	}  else if (strncmp (object, "add", MAX_BUFFER) == 0) {
+		// return add_device_to_bridge ();
+	}
 
-	return -1; 
+	return 1; 
 }
 
 
@@ -61,31 +73,49 @@ int handle_tap (char * arguments[], int length) {
 
 	// XXX: Implement these
 	if (strncmp(object, "create", MAX_BUFFER) == 0) {
-		return create_device (arguments[1]) != NULL;
+		return  create_device (arguments[1]) == NULL; 
 	} else if (strncmp (object, "show", MAX_BUFFER) == 0) {
-		return show_device (); 
+		return show_device (arguments[1]); 
 	} else if (strncmp (object, "delete", MAX_BUFFER) == 0) {
-		return remove_device (); 
-	} 
+		// return remove_device (); 
+	} else return 1; 
 
-	return -1; 
+	return 1; 
 }
 
-brx_device* create_device (char *name) {
+int show_device (char *tap_name) {
 
-	if (!name) return NULL; 
-	
+
+}
+
+
+brx_device* create_device (char *tap_name) {
+
+	if (!tap_name) return NULL; 
+
+	// XXX: There is a very small chance that malloc fails	
 	brx_device *dev = malloc (sizeof(brx_device));
+	if (!dev) return NULL;  
 
 	// XXX: Should be strncpy ??? 	
-	dev->info.interface_name = name; 	
-	dev->info.fd = tap_alloc (tap_name);
-	if (dev->info.tap_fd < 0) {
+	dev->info.interface_name = tap_name; 	
+
+	dev->tap_fd = tap_alloc (tap_name);
+	if (dev->tap_fd < 0) {
 		printf ("failed to allocate tap device"); 
 		return NULL;
 	}
+	
+	printf ("%s\n", tap_name); 
 
+	// XXX: Make a series of system calls using ioctl to get 
+	// information about the tap device 
+	set_device_metadata (dev); 
 	return dev; 
-	
-	
+}
+
+
+void set_device_metadata (brx_device *dev) {
+	// TODO: Implement
+	return;
 }
