3a898eb79bce83ea167f418ed598da0210986ca0
shale <slucas001@citymail.cuny.edu>
2026-08-12 05:03:52 -0400

    get dev by name, but bad implementation though

--- device.c
+++ device.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -13,14 +14,30 @@ char * brx_show_ip (unsigned char * ip) {
 	return NULL;
 }
 
+brx_device * get_device_by_name (brx_device ** devices, const char * name) {
+	if (!devices) return NULL;
+	
+	size_t idx = 0; 
+	brx_device * current = devices[idx]; 
+	while (devices[idx] != NULL) {
+		if (strncmp (name, current->info.interface_name, MAX_BUFFER) == 0) return current; 
+		idx += 1; 
+	}	
+
+	return NULL;
+}
+
 int show_device (char *tap_name) {
 
 	if (!tap_name) return 1; 
 
-	brx_device_info * info = get_device_metadata (...); 
+	brx_device_info * info = get_device_metadata (
+		get_device_by_name (tap_name, get_devices (bridge))
+	); 
+
 	if (!info) return 1; 
 
-	printf ("Device Name: [%s]\n", tap_name);
+	printf ("Device Name: [%s]\n", info->interface_name);
 	printf ("MAC Address: [%s]\n", info->mac_address);
 	printf ("IP  Address: [%s]\n", brx_show_ip(info->ip_address));
 	printf ("MTU:         [%zu]\n",info->mtu);
@@ -39,7 +56,7 @@ brx_device * create_device (char *tap_name) {
 
 	// XXX: Should be strncpy ??? 	
 	dev->info.interface_name = tap_name; 	
-	dev->ref_count = 1; 
+	dev->ref_count++; 
 
 	dev->tap_fd = tap_alloc (tap_name);
 	if (dev->tap_fd < 0) {
@@ -68,7 +85,6 @@ void free_device (brx_device * dev) {
 
 	return;
 }
-
 
 void set_device_metadata (brx_device *dev) {
 	// TODO: Implement
