7f6b0a7221ced2514556ed442b7315638f7c684e
shale <slucas001@citymail.cuny.edu>
2026-08-25 00:22:26 -0400

    new type added into the mix, used for passing the messages about state of the links to the server and client

--- brx.h
+++ brx.h
@@ -76,6 +76,23 @@ typedef struct brx_bridge  {
 } brx_bridge; 
 
 
+
+// Types of messages ... 
+enum message_type {
+	CREATE,
+	DELETE,
+	SHOW,
+	SUCCESS,
+	ERROR
+}
+
+// XXX: Passed across the UDS and is used for setting up control plane stuff ... 
+typedef struct brx_control_message {
+	size_t length;
+	message_type type;
+} brx_control_message; 
+
+
 brx_device * create_device (char * name);
 void free_device (brx_device * dev); 
 
